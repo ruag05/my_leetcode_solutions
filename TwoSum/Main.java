0477140516216a3a9b26ee871e222427d0052fff
Ruchir Agarwal <60838010+ruchiragarwal@users.noreply.github.com>
2021-06-24 23:23:14 -0700

    Added problem description

--- TwoSum/Main.java
+++ TwoSum/Main.java
@@ -1,10 +1,18 @@
 package com.company;
 
+//PROBLEM
+//  -Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//  -You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//  -You can return the answer in any order.
+
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(twoSum(nums, target);
     }
 
     public static int[] twoSum(int[] nums, int target) {
