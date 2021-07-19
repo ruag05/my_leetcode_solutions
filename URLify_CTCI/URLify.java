6d11e4db9c9a109574e0ead8236ebbaf6e63fd22
Ruchir Agarwal <60838010+ruchiragarwal@users.noreply.github.com>
2021-07-19 16:47:22 -0700

    Added code for 'URLify'
    Approach:
    1. Traverse from right to left.
    2. Use two pointers, one to mark where the word starts and other, to mark where the word ends. 
    3. Shift the word to the rightmost available positions.
    4. Insert %20 at the current pointer positions

---
+++ URLify_CTCI/URLify.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Arrays;
+
+public interface URLify {
+
+    //PROBLEM : URLify
+    //  -Write a method to replace all spaces in a string with '%20'. You may assume that the string
+    //  has sufficient space at the end to hold the additional characters, and that you are given the "true"
+    //  length of the string. (Note: If implementing in Java, please use a character array so that you can
+    //  perform this operation in place.)
+
+    public static void main(String[] args) {
+        String url = " Mr John Smith      ";
+        System.out.println(urlify(url, 14));
+    }
+    static String urlify(String s, int actualLength){
+        char[] chArr = s.toCharArray();
+        int k = 0;
+
+        for(int i = actualLength - 1; i >= 0; i--){
+            char c = chArr[i];
+            int endIndex = i;
+
+            //reach the space after the previous word
+            while(c != ' ') {
+                i--;
+                if(i < 0) return Arrays.toString(chArr);
+                c = chArr[i];
+            }
+
+            //shift word to the rightmost available positions
+            while(endIndex > i)
+                chArr[chArr.length - 1 - k++] = chArr[endIndex--];
+
+            //insert %20 in blank space formed in between by shifting the word
+            chArr[chArr.length - 1 - k++] = '0';
+            chArr[chArr.length - 1 - k++] = '2';
+            chArr[chArr.length - 1 - k++] = '%';
+
+        }
+        return Arrays.toString(chArr);
+    }
+}
