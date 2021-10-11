d74899fa6ae23e3e5ec23ba7a9be07545f98844d
Ruchir Agarwal <60838010+ruag05@users.noreply.github.com>
2021-10-11 00:48:17 -0700

    Added another approach
    Solution using recursion

--- ReverseLinkedList_[206]/ReverseLinkedList.java
+++ ReverseLinkedList_[206]/ReverseLinkedList.java
@@ -41,4 +41,20 @@ public class ReverseLinkedList {
 
         return head;
     }
+    
+    // time complexity: O(N)
+    // space complexity: O(1)
+    //took 0 ms (100 percentile) and 39.4 MB (30 percentile)
+    //[Recursion] Using recursion, move till the very last node and keep returning that so that the new head is the last node
+    //Then, set the 'next' attribute of the next node of current head to point to head. Doing this will reverse the direction
+    //of the list.
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
 }
