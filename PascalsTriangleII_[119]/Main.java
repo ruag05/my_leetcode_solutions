cee08208be247eb31e598e638377ef9fb66605db
Ruchir Agarwal <60838010+ruag05@users.noreply.github.com>
2021-10-19 10:56:29 -0700

    Added approach#2 and shifted the previous approach#2 to approach#3

--- PascalsTriangleII_[119]/Main.java
+++ PascalsTriangleII_[119]/Main.java
@@ -36,11 +36,64 @@ public class Main {
     }
 
     ///////////////////////////
-
     //approach#2
+    //  time complexity: O(k^2)
+    //  space complexity: O(k)
+    //took 63 ms (2 percentile) and 38.9 MB (8 percentile)
+    //[recursion] Build a secondary array and fill it with default 0 values. Then using recursion store each of computed
+    //values in a Map with that {rowIndex, colIndex} as key and computed value as Value
+    private final class RowCol {
+        private int row, col;
+
+        public RowCol(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = (int) (row ^ (row >>> 32));
+            return (result << 5) - 1 + (int) (col ^ (col >>> 32)); // 31 * result == (result << 5) - 1
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null) return false;
+            if (this.getClass() != o.getClass()) return false;
+
+            RowCol rowCol = (RowCol) o;
+            return row == rowCol.row && col == rowCol.col;
+        }
+    }
+
+    private Map<RowCol, Integer> cache = new HashMap<>();
+
+    private int getNum2(int row, int col) {
+        RowCol rowCol = new RowCol(row, col);
+        if (cache.containsKey(rowCol)) {
+            return cache.get(rowCol);
+        }
+        int computedVal =
+                (row == 0 || col == 0 || row == col) ? 1 : getNum2(row - 1, col - 1) + getNum2(row - 1, col);
+        cache.put(rowCol, computedVal);
+        return computedVal;
+    }
+
+    public List<Integer> getRow2(int rowIndex) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            ans.add(getNum2(rowIndex, i));
+        }
+        return ans;
+    }
+    
+    ///////////////////////////
+
+    //approach#3
     // took 0 ms (100 percentile) and 36.5 MB (91 percentile) (generates n^th row elements without generating
     // previous rows, uses relationship between the current element and the previous element in the same row)
-    public static List<Integer> getRow2(int rowIndex){
+    public static List<Integer> getRow3(int rowIndex){
         Integer[] result = new Integer[rowIndex + 1];
         result[0] = 1;
         for(int i = 1; i < result.length; i++)
